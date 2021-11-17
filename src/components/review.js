1f9565d7244267cacbed8e485a03eaf854606c22
thanhnguyen10101992 <49136824+thanhnguyen10101992@users.noreply.github.com>
2021-11-17 11:18:19 +0700

    update blank comment review

--- src/components/review.js
+++ src/components/review.js
@@ -58,13 +58,13 @@ export default function ReviewContentApp( props ) {
       dataReview.reviewContent.length > 0 &&
       <div className="wrapper-review-lst">
 
-            <h3>Community Reviews sss</h3>
+            <h3>Community Reviewsss</h3>
 
       {
         dataReview.reviewContent &&
         dataReview.reviewContent.length > 0 &&
         dataReview.reviewContent.map( data => {
-          if(data.parent == 0) {
+          if(data.parent == 0 && data.comment.length>0) {
             return <Reviewdata parent={data.parent} name_author={ data.name } comment_rv={ data.comment }  id_review = { data.id_reviews } url_avatar= {data.url_avatar}  date_comment={data.date_coment} />
           }
         } )
